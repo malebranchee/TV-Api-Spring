67961e4622159a033f36f2f9257c657f144b2d5d
malebranchee <paho3998@mail.ru>
2024-04-13 16:37:27 +0600

    Added validation of input forms

--- src/main/java/com/example/lab5/Repositories/Entities/TvEntity.java
+++ src/main/java/com/example/lab5/Repositories/Entities/TvEntity.java
@@ -23,39 +23,37 @@ public class TvEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
-    @NotNull(message = "Поле не должно быть пустым")
     @PositiveOrZero(message = "Здесь должно быть число 0 или больше")
     private int id;
 
 
 
     @Column(name = "product_name")
-    @NotNull(message = "Поле 'name' не должно быть пустым!")
+    @NotBlank(message = "Поле не должно быть пустым!")
     @Size(min = 1, max = 30, message = "Размер поля должен быть от 2 до 30 символов")
     private String name;
 
 
     @Column(name = "type")
-    @NotNull(message = "Поле 'type' не должно быть пустым!")
+    @NotBlank(message = "Поле не должно быть пустым!")
     @Size(min = 1, max = 30, message = "Размер поля должен быть от 2 до 30 символов")
     private String type;
 
     @Column(name = "brand")
-    @NotNull(message = "Поле 'brand' не должно быть пустым!")
+    @NotBlank(message = "Поле не должно быть пустым!")
     @Size(min = 1, max = 30)
     private String brand;
 
-
-    @PositiveOrZero(message = "Здесь должно быть число 0 или больше")
     @Column(name = "screen_diagonal")
+    @PositiveOrZero(message = "Здесь должно быть число 0 или больше")
     private int size;
 
-    @PositiveOrZero(message = "Здесь должно быть число 0 или больше")
     @Column(name = "price")
+    @PositiveOrZero(message = "Здесь должно быть число 0 или больше")
     private double price;
 
-    @PositiveOrZero(message = "Здесь должно быть число 0 или больше")
     @Column(name = "quantity")
+    @PositiveOrZero(message = "Здесь должно быть число 0 или больше")
     private int quantity;
 
     public TvEntity(int id, String name, String type, String brand, int size, double price, int quantity) {
