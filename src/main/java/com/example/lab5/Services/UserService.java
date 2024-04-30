97a2d39b5b324052a691dbe041eae1cf9961e5f6
malebranchee <paho3998@mail.ru>
2024-04-30 15:45:06 +0600

    Added admin settings configuration

--- src/main/java/com/example/lab5/Services/UserService.java
+++ src/main/java/com/example/lab5/Services/UserService.java
@@ -7,6 +7,7 @@ import com.example.lab5.Repositories.UserRepository;
 import com.example.lab5.Dto.UserDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -27,6 +28,7 @@ public class UserService implements UserDetailsService {
     RoleRepository roleRepository;
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
+
 
 
     @Override
